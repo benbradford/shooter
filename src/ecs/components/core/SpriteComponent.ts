e65cdaaa57b9d22fadd3407f806ff876a473c048
Ben Bradford <bebradfo@amazon.com>
2026-02-07 19:19:47 +0000

    Commiting before making a large change

--- src/ecs/components/core/SpriteComponent.ts
+++ src/ecs/components/core/SpriteComponent.ts
@@ -3,17 +3,38 @@ import type { Component } from '../../Component';
 import type { Entity } from '../../Entity';
 import type { TransformComponent } from './TransformComponent';
 
+export type SpriteComponentProps = {
+  offsetXPx?: number;
+  offsetYPx?: number;
+}
+
 export class SpriteComponent implements Component {
   entity!: Entity;
   readonly sprite: Phaser.GameObjects.Sprite;
+  private readonly offsetXPx: number;
+  private readonly offsetYPx: number;
 
-  constructor(scene: Phaser.Scene, texture: string, private readonly transformComp: TransformComponent) {
-    this.sprite = scene.add.sprite(transformComp.x, transformComp.y, texture);
+  constructor(
+    scene: Phaser.Scene,
+    texture: string,
+    private readonly transformComp: TransformComponent,
+    props: SpriteComponentProps = {}
+  ) {
+    this.offsetXPx = props.offsetXPx ?? 0;
+    this.offsetYPx = props.offsetYPx ?? 0;
+    this.sprite = scene.add.sprite(
+      transformComp.x + this.offsetXPx,
+      transformComp.y + this.offsetYPx,
+      texture
+    );
     this.sprite.setScale(transformComp.scale);
   }
 
   update(_delta: number): void {
-    this.sprite.setPosition(this.transformComp.x, this.transformComp.y);
+    this.sprite.setPosition(
+      this.transformComp.x + this.offsetXPx,
+      this.transformComp.y + this.offsetYPx
+    );
     this.sprite.setRotation(this.transformComp.rotation);
     this.sprite.setScale(this.transformComp.scale);
   }
